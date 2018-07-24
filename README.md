58fe08baa6d474d0c4cdda92a9ef60aa0ed84d22
NEGRONN <33583695+NEGRONN@users.noreply.github.com>
2018-07-24 18:25:08 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# atom
-hayatı tesbih yapmışam
+# SGB
+SGB is a discord bot made for awesome general stuffs.It is made with discord.js
+
---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const RichEmbed = new Discord.RichEmbed();
+var fs = require('fs');
+
+// -->COMMANDS<--
+
+bot.on('guildMemberAdd',member =>{
+    member.send('**NS PROJESI**https://discord.gg/vnrq6kQ\n**HAKKIMIZDA** https://goo.gl/8YDmqa\n**COVER**https://ibb.co/gfbpZJ');
+
+    //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
+
+});
+
+
+bot.on('guildMemberRemove', member=>{
+   
+});
+
+
+
+bot.on('ready',()=> {
+console.log('NEGRONN');
+
+});
+
+bot.login("NDcxMDY3OTc5MTk1NjEzMjE1.Djfdug.icb1O6RgF5bkZ4XraQgOALdNkTA");
