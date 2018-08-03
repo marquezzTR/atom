68466f1de7387747e0be39b713d8145dbc55b505
marquezzTR <41957108+marquezzTR@users.noreply.github.com>
2018-08-04 02:40:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send('**Merhaba Dostum, Eğer Buraya Gelirsen Beni Çok Mutlu Edersin <3 ** https://discord.gg/4kSdf5B');
+    member.send('**Merhaba Dostum, \nEğer Buraya Gelirsen Beni Çok Mutlu Edersin \nKayıt Yaptırmayı Unutma Lütfen ** https://discord.gg/4kSdf5B');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
