50b16a0cdf00e331d01d0f35d881f2d04f6b8565
marquezzTR <41957108+marquezzTR@users.noreply.github.com>
2018-08-04 17:27:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send('**Merhaba Şekerim, \nEğer Buraya Gelirsen \nTelefondan Karşılıklı Konuşuruz \nKayıt Yaptırmayı Unutma Lütfen ** https://discord.gg/4kSdf5B');
+    member.send('**Merhaba Şekerim, \nEğer Buraya Gelirsen \n Karşılıklı Konuşuruz \nKayıt Yaptırmayı Unutma Lütfenn  ** https://discord.gg/4kSdf5B');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
